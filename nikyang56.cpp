42add3900a396a1faf8d61cafd29ddd287a788b5
nikishinasandra-creator <nikishinasandra@gmail.com>
2026-02-07 10:02:54 +0300

    pr1

---
+++ nikyang56.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(0, "");
+
+	int n_1 = 22;
+	int n_2 = 43;
+	int n_3 = 65;
+	int n_4 = 13;
+	int n_5 = 97;
+
+	cout << "[INT]" << endl;
+	cout << "n_1 = " << n_1 << " | тип: int, размер: 4 байта" << endl;
+	cout << "n_2 = " << n_2 << " | тип: int, размер: 4 байта" << endl;
+	cout << "n_3 = " << n_3 << " | тип: int, размер: 4 байта" << endl;
+	cout << "n_4 = " << n_4 << " | тип: int, размер: 4 байта" << endl;
+	cout << "n_5 = " << n_5 << " | тип: int, размер: 4 байта" << endl << endl;
+
+	short sh_1 = 11;
+	short sh_2 = 12;
+	short sh_3 = 13;
+	short sh_4 = 14;
+	short sh_5 = 15;
+
+	cout << "[SHORT]" << endl;
+	cout << "sh_1 = " << sh_1 << " | тип: short, размер: 2 байта" << endl;
+	cout << "sh_2 = " << sh_2 << " | тип: short, размер: 2 байта" << endl;
+	cout << "sh_3 = " << sh_3 << " | тип: short, размер: 2 байта" << endl;
+	cout << "sh_4 = " << sh_4 << " | тип: short, размер: 2 байта" << endl;
+	cout << "sh_5 = " << sh_5 << " | тип: short, размер: 2 байта" << endl << endl;
+
+	float f_1 = 1.1;
+	float f_2 = 1.2;
+	float f_3 = 1.3;
+	float f_4 = 1.4;
+	float f_5 = 1.5;
+
+	cout << "[FLOAT]" << endl;
+	cout << "f_1 = " << f_1 << " | тип: float, размер: 4 байта" << endl;
+	cout << "f_2 = " << f_2 << " | тип: float, размер: 4 байта" << endl;
+	cout << "f_3 = " << f_3 << " | тип: float, размер: 4 байта" << endl;
+	cout << "f_4 = " << f_4 << " | тип: float, размер: 4 байта" << endl;
+	cout << "f_5 = " << f_5 << " | тип: float, размер: 4 байта" << endl << endl;
+
+	double d_1 = 11.1;
+	double d_2 = 21.2;
+	double d_3 = 31.3;
+	double d_4 = 41.4;
+	double d_5 = 51.5;
+
+	cout << "[DOUBLE]" << endl;
+	cout << "d_1 = " << f_1 << " | тип: float, размер: 8 байта" << endl;
+	cout << "d_2 = " << f_2 << " | тип: float, размер: 8 байта" << endl;
+	cout << "d_3 = " << f_3 << " | тип: float, размер: 8 байта" << endl;
+	cout << "d_4 = " << f_4 << " | тип: float, размер: 8 байта" << endl;
+	cout << "d_5 = " << f_5 << " | тип: float, размер: 8 байта" << endl << endl;
+
+	char ch_1 = 11.1;
+	char ch_2 = 21.2;
+	char ch_3 = 31.3;
+	char ch_4 = 41.4;
+	char ch_5 = 51.5;
+
+	cout << "[DOUBLE]" << endl;
+	cout << "d_1 = " << f_1 << " | тип: float, размер: 8 байта" << endl;
+	cout << "d_2 = " << f_2 << " | тип: float, размер: 8 байта" << endl;
+	cout << "d_3 = " << f_3 << " | тип: float, размер: 8 байта" << endl;
+	cout << "d_4 = " << f_4 << " | тип: float, размер: 8 байта" << endl;
+	cout << "d_5 = " << f_5 << " | тип: float, размер: 8 байта" << endl << endl;
+
+	int _; cin >> _;
+
+}
