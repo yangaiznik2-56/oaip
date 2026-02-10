929b026f244dde809acd5503c6607495f8f119f8
yanglyaeva aiziatullova nikishina 2isp9-56 <nikishinasandra@gmail.com>
2026-02-10 12:37:55 +0300

    Update pr6.cpp

--- pr6.cpp
+++ pr6.cpp
@@ -2,41 +2,44 @@
 using namespace std;
 
 int main() {
-	int m;
-	setlocale(0, "");
+ setlocale(0, "");
+ int mon;
 
-	cout << "Месяца года\n";
-	cout << "[ 1 ] Январь\n";
-	cout << "[ 2 ] Февраль\n";
-	cout << "[ 3 ] Март\n";
-	cout << "[ 4 ] Апрель\n";
-	cout << "[ 5 ] Май\n";
-	cout << "[ 6 ] Июнь\n";
-	cout << "[ 7 ] Июль\n";
-	cout << "[ 8 ] Август\n";
-	cout << "[ 9 ] Сентябрь\n";
-	cout << "[ 10 ] Октябрь\n";
-	cout << "[ 11 ] Ноябрь\n";
-	cout << "[ 12 ] Декабрь\n";
+ cout << "[ + ] Месяца года" << endl;
+ cout << "[ 1 ] Январь" << endl;
+ cout << "[ 2 ] Февраль" << endl;
+ cout << "[ 3 ] Март" << endl;
+ cout << "[ 4 ] Апрель" << endl;
+ cout << "[ 5 ] Май" << endl;
+ cout << "[ 6 ] Июнь" << endl;
+ cout << "[ 7 ] Июль" << endl;
+ cout << "[ 8 ] Август" << endl;
+ cout << "[ 9 ] Сентябрь" << endl;
+ cout << "[ 10 ] Октябрь" << endl;
+ cout << "[ 11 ] Ноябрь" << endl;
+ cout << "[ 12 ] Декабрь" << endl;
 
-	cout << "Выберите месяц: ";
-	cin >> m;
+ cout << "[ + ] Выберите месяц: ";
+ cin >> mon;
 
-	switch (m) {
-	case 1:  cout << "Выбран месяц \"Январь\""; break;
-	case 2:  cout << "Выбран месяц \"Февраль\""; break;
-	case 3:  cout << "Выбран месяц \"Март\""; break;
-	case 4:  cout << "Выбран месяц \"Апрель\""; break;
-	case 5:  cout << "Выбран месяц \"Май\""; break;
-	case 6:  cout << "Выбран месяц \"Июнь\""; break;
-	case 7:  cout << "Выбран месяц \"Июль\""; break;
-	case 8:  cout << "Выбран месяц \"Август\""; break;
-	case 9:  cout << "Выбран месяц \"Сентябрь\""; break;
-	case 10: cout << "Выбран месяц \"Октябрь\""; break;
-	case 11: cout << "Выбран месяц \"Ноябрь\""; break;
-	case 12: cout << "Выбран месяц \"Декабрь\""; break;
-	default: cout << "Не правильно введен номер месяца!"; break;
-	}
+ switch (mon) {
+ case 1:  cout << "[ + ] Выбран месяц \"Январь\""; break;
+ case 2:  cout << "[ + ] Выбран месяц \"Февраль\""; break;
+ case 3:  cout << "[ + ] Выбран месяц \"Март\""; break;
+ case 4:  cout << "[ + ] Выбран месяц \"Апрель\""; break;
+ case 5:  cout << "[ + ] Выбран месяц \"Май\""; break;
+ case 6:  cout << "[ + ] Выбран месяц \"Июнь\""; break;
+ case 7:  cout << "[ + ] Выбран месяц \"Июль\""; break;
+ case 8:  cout << "[ + ] Выбран месяц \"Август\""; break;
+ case 9:  cout << "[ + ] Выбран месяц \"Сентябрь\""; break;
+ case 10: cout << "[ + ] Выбран месяц \"Октябрь\""; break;
+ case 11: cout << "[ + ] Выбран месяц \"Ноябрь\""; break;
+ case 12: cout << "[ + ] Выбран месяц \"Декабрь\""; break;
+ default: cout << "[ + ] Не правильно введен номер месяца!"; break;
+ }
 
-	return 0;
+ return 0;
+
+ int _;
+ cin >> _;
 }
